582c0ca6c985d62be5dcdbfc8cda9cedf20f7b76
katta-sravani14 <kattasravani413@gmail.com>
2021-01-21 19:23:23 +0530

    day10

---
+++ arthmetic/inputuc1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+echo "Enter a"
+read  a
+echo "b"
+read  b
+echo "c"
+read  c
+
+
---
+++ arthmetic/uc2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+a=7
+b=8
+c=5
+num=$(( $expr $a + $b * $c ))
+echo $num
+
+
+#!/bin/bash -x
+a=7
+b=8
+c=5
+num=$(( $expr $a + $b * $c ))
+echo $num
